a445f0502347927e71851204dd204930ee8724e8
Peter Anema <7320173+PeterAnema@users.noreply.github.com>
2021-03-23 10:01:39 +0100

    Update

---
+++ sandbox/guessing-game.py
@@ -0,0 +1,25 @@
+import random
+
+lower = 1
+upper = 100
+
+magic_number = random.randint(lower, upper)
+
+print(f'Guess a number between {lower} and {upper}')
+
+number_of_guesses = 0
+while True:
+
+    guess = int(input('What is your next guess? '))
+
+    number_of_guesses += 1
+
+    if guess > magic_number:
+        print('lower ...')
+
+    elif guess < magic_number:
+        print('higher ...')
+
+    elif guess == magic_number:
+        print(f'YEAAAH! You guessed it in {number_of_guesses} guesses')
+        break
