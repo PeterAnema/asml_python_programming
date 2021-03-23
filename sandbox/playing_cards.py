9068ce74fe4b92c3500f31b3e0c32d9d4f2f992a
Peter Anema <7320173+PeterAnema@users.noreply.github.com>
2021-03-23 13:41:58 +0100

    Update

--- sandbox/playing_cards.py
+++ sandbox/playing_cards.py
@@ -1,13 +1,13 @@
 import random
 
-# suits = 'clubs diamonds hearts spades'.split()
+suits = 'clubs diamonds hearts spades'.split()
 # suits = ('\u2660', '\u2661', '\u2662', '\u2663')
-suits = ('♣', '♢', '♡', '♠')
+# suits = ('♣', '♢', '♡', '♠')
 
 ranks = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
 
-# cards = [f'{r} of {s}' for r in ranks for s in suits]
-cards = [f'{s}{r}' for r in ranks for s in suits]
+cards = [f'{r} of {s}' for r in ranks for s in suits]
+# cards = [f'{s}{r}' for r in ranks for s in suits]
 
 random.shuffle(cards)
 
