fdca9a09723083a7e8daf34fe8ec8bd594f61083
Peter Anema <7320173+PeterAnema@users.noreply.github.com>
2021-03-25 11:16:28 +0100

    Update

--- sandbox/demo_urllib.py
+++ sandbox/demo_urllib.py
@@ -1,7 +1,7 @@
 import urllib.request
 import json
 
-city = 'New york'
+city = 'amsterdam'
 
 url = 'http://api.openweathermap.org/data/2.5/weather?q=%s&appid=d1526a9039658a6f76950cff21823aff&units=metric' % city
 
